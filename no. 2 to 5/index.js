18c2f2133090bef3a4e957b5408f10adad02df49
firmanssssss <avenged.firman33@gmail.com>
2022-12-28 01:56:18 +0700

    add new file no.5 and some new features in folder no.2 to 5

--- no. 2 to 5/index.js
+++ no. 2 to 5/index.js
@@ -67,6 +67,22 @@ form.addEventListener("submit", () => {
   fetchData();
 });
 
+//navbar
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((element) => {
+  element.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  });
+});
+
 // form.addEventListener("submit", (event) => {
 //   event.preventDefault();
 //   const name = form.elements.name.value;
--- no.5/script.js
+++ no.5/script.js
@@ -5,3 +5,10 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
+
+document.querySelectorAll(".nav-link").forEach((element) => {
+  element.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  });
+});
